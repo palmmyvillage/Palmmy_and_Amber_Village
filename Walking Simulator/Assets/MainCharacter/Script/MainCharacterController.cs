46c23598ce993af08b6e2b7a810b7b9d2ae8dab3
Temmie <ps3808@nyu.edu>
2018-10-10 18:36:20 -0400

    Palmmy Castle Model + FPS

---
+++ Walking Simulator/Assets/MainCharacter/Script/MainCharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour{
+
+	public float ResponseSpeed = 1.0f;
+	public float WalkingSpeed = 1.0f;
+	private Vector3 contactPoint;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float translation = Input.GetAxis("Vertical")*ResponseSpeed;
+		float straffe = Input.GetAxis("Horizontal")*ResponseSpeed;
+		translation *= WalkingSpeed*Time.deltaTime;
+		straffe *= WalkingSpeed*Time.deltaTime;
+		
+		transform.Translate(straffe,0,translation);
+		
+		if (Input.GetKeyDown("escape"))
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+	
+	void OnControllerColliderHit (ControllerColliderHit hit) {
+		contactPoint = hit.point;
+	}
+	
+	// Store point that we're in contact with for use in FixedUpdate if needed
+}

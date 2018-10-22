7b29605631a54993d806127ab601302d959a121b
Amber <gl1697@gmail.com>
2018-10-21 21:33:34 -0400

    Amber adds town scene. Amber adds some new materials.

---
+++ Walking Simulator/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+	public Transform target;
+	void Update () {
+		if (target != null)
+			transform.rotation = target.rotation;
+	}
+}

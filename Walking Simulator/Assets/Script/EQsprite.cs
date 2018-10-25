f12b78cbcbd9914bcca63a78033699b35bacbebb
Palmmy <ps3808@nyu.edu>
2018-10-25 17:35:26 -0400

    Palmmy deal with sword and shield

--- Walking Simulator/Assets/Script/EQsprite.cs
+++ Walking Simulator/Assets/Script/EQsprite.cs
@@ -15,18 +15,21 @@ public class EQsprite : MonoBehaviour {
 	//Set timer for sprite change
 	private float SpriteTimer = 0.0f;
 	private float posX;
+	private float posY;
 	private float posZ;
 	
 	// Use this for initialization
 	void Start ()
 	{
-		posX = gameObject.GetComponent<Transform>().position.x;
-		posZ = gameObject.GetComponent<Transform>().position.z;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		posX = gameObject.GetComponent<RectTransform>().position.x;
+		posY = gameObject.GetComponent<RectTransform>().position.y;
+		posZ = gameObject.GetComponent<RectTransform>().position.z;
+		
 		// increase timer every frmae
 		SpriteTimer += Time.deltaTime;
 		
@@ -36,7 +39,8 @@ public class EQsprite : MonoBehaviour {
 			//set timer limit to change sprite
 			if (SpriteTimer >= 0.5f)
 			{
-				gameObject.transform.position = new Vector3(posX, Up, posZ);
+				gameObject.transform.position = new Vector3(posX, posY + Up, posZ);
+				
 				SpriteTimer = 0.0f;
 				Sprite01Checked = false;
 			} 
@@ -47,7 +51,7 @@ public class EQsprite : MonoBehaviour {
 			//set timer limit to change sprite
 			if (SpriteTimer >= 0.5f)
 			{
-				gameObject.transform.position = new Vector3(posX, Down, posZ);
+				gameObject.transform.position = new Vector3(posX, posY - Down, posZ);
 				SpriteTimer = 0.0f;
 				Sprite01Checked = true;
 			} 

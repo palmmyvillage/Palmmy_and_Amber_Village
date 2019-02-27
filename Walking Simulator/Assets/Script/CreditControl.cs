1c7a08eade17779547b5745a8e92ee2ca195103e
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-02-27 02:41:52 -0500

    Update Fix everything

---
+++ Walking Simulator/Assets/Script/CreditControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditControl : MonoBehaviour
+{
+    public GameObject[] Credit;
+    
+    void Start()
+    {
+        StartCoroutine(PlayCredit());
+    }
+
+    IEnumerator PlayCredit()
+    {
+        Credit[0].SetActive(true);
+        yield return new WaitForSeconds(3);
+        Credit[0].SetActive(false);
+        Credit[1].SetActive(true);
+        yield return new WaitForSeconds(6);
+        Credit[1].SetActive(false);
+        Credit[2].SetActive(true);
+        yield return new WaitForSeconds(6);
+        Credit[2].SetActive(false);
+        Credit[3].SetActive(true);
+    }
+    
+    
+}
